39f213da57fe3d17bb4db222d7c87ad6c88576be
Ofijo79 <oriolfijo2004@gmail.com>
2024-05-28 12:47:23 +0200

    cositas

--- Assets/Scripts/MenuScripts/Pause.cs
+++ Assets/Scripts/MenuScripts/Pause.cs
@@ -1,21 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Pause : MonoBehaviour
 {
-    
     private bool isPaused = false;
     public GameObject canva;
+    public GameObject Resume;
 
     void Start()
     {
         Time.timeScale = 1f;
+        Cursor.visible = false;
     }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Pause"))
+        if (Input.GetButtonDown("Pause") || Input.GetKeyDown(KeyCode.Escape))
         {
             TogglePause();
         }
@@ -27,13 +29,17 @@ public class Pause : MonoBehaviour
 
         if (isPaused)
         {
+            Cursor.visible = true;
             canva.SetActive(true);
             Time.timeScale = 0f;
+            EventSystem.current.SetSelectedGameObject(Resume);
         }
         else
         {
             canva.SetActive(false);
             Time.timeScale = 1f;
+            Cursor.visible = false;
+            EventSystem.current.SetSelectedGameObject(null);
         }
     }
 
